848ed4c739f550d2e0f1037db6ef6676cd907a91
Chanveasna ENG <2023488eng@aupp.edu.kh>
2026-03-15 17:41:58 +0700

    add post request to root

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/', (req, res)=> {
     res.send("<h1>Assignment 3</h1>")
 })
+app.post('/', (req, res)=> {
+    res.send("Received!")
+})
 
 app.listen(5000, () =>
 console.log('EXPRESS Server Started at Port No: 5000'));
